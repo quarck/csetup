8975c996d7405b98e231bf4ea430e5725be3a90b
Sergey Parshin <quarck@gmail.com>
2012-10-31 14:57:47 +0000

    Added libpng library

--- csetup/jni/Android.mk
+++ csetup/jni/Android.mk
@@ -4,6 +4,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := csetup 
-LOCAL_SRC_FILES := csetup.cpp
+LOCAL_SRC_FILES := \
+	libpng/pngwutil.c libpng/pngwtran.c libpng/pngwrite.c libpng/pngwio.c libpng/pngtrans.c \
+	libpng/pngset.c libpng/pngrutil.c libpng/pngrtran.c libpng/pngrio.c libpng/pngread.c \
+	libpng/pngpread.c libpng/pngmem.c libpng/pngget.c libpng/pngerror.c libpng/png.c \
+	csetup.cpp
+
+LOCAL_CFLAGS :=-I./libpng/
+
 LOCAL_LDLIBS := -static -lm -lc -lstdc++ 
 include $(BUILD_EXECUTABLE)
